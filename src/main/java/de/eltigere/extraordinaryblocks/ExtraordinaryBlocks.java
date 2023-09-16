55b3d63378d6072461d44e1e8620f1483012b12b
El_Tigere <eltigere@web.de>
2023-09-16 14:00:21 +0200

    added BER registration

--- src/main/java/de/eltigere/extraordinaryblocks/ExtraordinaryBlocks.java
+++ src/main/java/de/eltigere/extraordinaryblocks/ExtraordinaryBlocks.java
@@ -1,6 +1,11 @@
 package de.eltigere.extraordinaryblocks;
 
 import com.mojang.logging.LogUtils;
+import de.eltigere.extraordinaryblocks.blockentities.FogBlockBE;
+import de.eltigere.extraordinaryblocks.blockentities.renderers.FogBlockBER;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -18,14 +23,25 @@ public class ExtraordinaryBlocks {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
+        //modEventBus.addListener(ExtraordinaryBlocks::onRegisterNamedRenderTypes);
+        modEventBus.addListener(ExtraordinaryBlocks::onRegisterEntityRenderers);
 
         MinecraftForge.EVENT_BUS.register(this);
 
         // register mod content
         BlockRegistry.register(modEventBus);
+        BlockEntityRegistry.register(modEventBus);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("common setup of Extraordinary Blocks Mod");
+    }
+
+    /*private static void onRegisterNamedRenderTypes(RegisterNamedRenderTypesEvent event) {
+        event.register("fog", RenderType.solid(), Sheets.solidBlockSheet());
+    }*/
+
+    private static void onRegisterEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(BlockEntityRegistry.FOG_BET.get(), FogBlockBER::new);
     }
 }
