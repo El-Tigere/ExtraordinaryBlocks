441f228ce90bf8232fcf9517f5cbf3158beeed4d
El_Tigere <eltigere@web.de>
2023-09-16 13:58:56 +0200

    added BlockEntity to Fog Block

--- src/main/java/de/eltigere/extraordinaryblocks/BlockEntityRegistry.java
+++ src/main/java/de/eltigere/extraordinaryblocks/BlockEntityRegistry.java
@@ -1,5 +1,6 @@
 package de.eltigere.extraordinaryblocks;
 
+import de.eltigere.extraordinaryblocks.blockentities.FogBlockBE;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -8,6 +9,8 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class BlockEntityRegistry {
     public static final DeferredRegister<BlockEntityType<?>> BETS = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, ExtraordinaryBlocks.MODID); // block entity types -> bets
+
+    public static final RegistryObject<BlockEntityType<FogBlockBE>> FOG_BET = BETS.register("fog_block", () -> BlockEntityType.Builder.of(FogBlockBE::new, BlockRegistry.FOG_BLOCK.get()).build(null));
 
     public static void register(IEventBus modEventBus) {
         BETS.register(modEventBus);
